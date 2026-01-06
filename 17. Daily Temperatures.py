9505f5eb516799c657408ccc481ad64854d571d8
Sidharth P Nair <sidharthpnair20@gmail.com>
2026-01-06 22:36:00 +0530

    Update 17. Daily Temperatures.py

--- 17. Daily Temperatures.py
+++ 17. Daily Temperatures.py
@@ -1,18 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-         n = len(temperatures)
-        answer = [0] * n       # result array
-        stack = []             # stack to store indices
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []
 
         for i in range(n):
-
-            # While current temperature is higher than
-            # the temperature at the index on top of the stack
             while stack and temperatures[i] > temperatures[stack[-1]]:
-                prev_day = stack.pop()
-                answer[prev_day] = i - prev_day
+                prev = stack.pop()
+                answer[prev] = i - prev
 
-            # Push current index onto stack
             stack.append(i)
 
         return answer
